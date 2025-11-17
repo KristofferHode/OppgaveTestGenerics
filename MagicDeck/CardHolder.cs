d3da5bc2b84994174ad22add5af90837acf80182
KristofferHode <kristoffer9.kodehode@gmail.com>
2025-11-17 09:32:32 +0100

    initial commit trying to fix runing error

---
+++ MagicDeck/CardHolder.cs
@@ -0,0 +1,26 @@
+namespace modul2Oppgave1;
+//gammel kode for å lage en deck holder som holder ett spesifikt antall kort
+public class CardHolder<T>
+{
+    private T[] cards = new T[6];
+    public void AddCard(T card, int index)
+    {
+        if (index < 0 || index >= 6)
+            throw new IndexOutOfRangeException("index must be between 0 and 5");
+        
+        cards[index] = card;
+        }
+
+    public T GetCard(int index)
+    {
+        return cards[index];
+    }
+
+    public void PrintAll()
+    {
+        foreach (var card in cards)
+        {
+            Console.WriteLine(card);
+        }
+    }
+}
---
+++ Magicdeck.test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit.Sdk;
+
+namespace modul2Oppgave1.test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Add_AddsCardToDeck()
+    {
+        var deck = new Deck<MagicCard>();
+        var card = new MagicCard("Ligthningbolt", "Instant", 1);
+    }
+}
